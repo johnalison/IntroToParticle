54db7fe5ce48517ce473298018c6f979d45403fa
John Alison <johnda102@gmail.com>
2020-03-04 19:25:45 -0500

    Update midterm

--- MidTerm/MidTermGrades.py
+++ MidTerm/MidTermGrades.py
@@ -6,55 +6,81 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
 
-grades_raw = [#20  # Kiran
-              24 # Josh Freudenhammer
-              ,16.5 # Steven HAll
-              ,19 # Josh Kim 
-              ,16.25 # Brandon N
-              ,21 # Ian Harris
-              ,17.5 # Max Perry
-              ,24.5 # Ruvini
-              ,22.75 # Ian Holst
-              ,28.5 # Naren
-              ,9, #Yue Xu
+grades_raw = [40.5,#Grayson 
+              15.  ,#Katherine
+              35.  ,#Kevin
+              44.  ,#Nick
+              26.  ,#Aileen
+              25.  ,#Dez 
+              30.  ,#Trevor
               ]
 
-home_work = [ #80
-              62.5 #Joshua
-              ,42.4 #Steven
-              ,90.7 #Joshua 
-              ,54.8 #Brandon
-              ,92.7 # Ian Harris
-              ,24,#Max
-              62.5,#Ruvini
-              80, #Ian Holst
-              100, #Naren
-              59,  # Yue
-              ]
+# in %
+home_work = [92.7,#Grayson 
+             73.4,#Katherine
+             79.8,#Kevin
+             96.8,#Nick
+             85.5,#Aileen
+             50.8,#Dez 
+             93.2,#Trevor
+             ]
+
+
+# 2019
+#grades_raw = [#20  # Kiran
+#              24 # Josh Freudenhammer
+#              ,16.5 # Steven HAll
+#              ,19 # Josh Kim 
+#              ,16.25 # Brandon N
+#              ,21 # Ian Harris
+#              ,17.5 # Max Perry
+#              ,24.5 # Ruvini
+#              ,22.75 # Ian Holst
+#              ,28.5 # Naren
+#              ,9, #Yue Xu
+#              ]
+## in %
+#home_work = [ #80
+#              62.5 #Joshua
+#              ,42.4 #Steven
+#              ,90.7 #Joshua 
+#              ,54.8 #Brandon
+#              ,92.7 # Ian Harris
+#              ,24,#Max
+#              62.5,#Ruvini
+#              80, #Ian Holst
+#              100, #Naren
+#              59,  # Yue
+#              ]
 
 combined = []
 for i in range(len(grades_raw)):
-    combined.append(round((grades_raw[i]/53*100 + home_work[i])/2,2))
+    print(str(i)+"==> "+str(round((grades_raw[i]/50*100 + home_work[i])/2,2)))
+    combined.append(round((grades_raw[i]/50*100 + home_work[i])/2,2))
+print(grades_raw)
+print(home_work)
 print(combined)
+
+
 
 grades = np.array(grades_raw)
 homework = np.array(home_work)
 #average = sum(grades)/len(grades_raw)
 average = round(np.average(grades),1)
 rms = round(np.std(grades),1)
-
+print(average)
 
 
 bins = np.linspace(0,50,25)
 
 plt.hist(grades, bins=bins,color='b', alpha=0.65, histtype='stepfilled', label='stepfilled hist')
-plt.plot([21,21],[0,3],'r--')
-plt.annotate("Mean = "+str(average), xy=(20, 2.5), xytext=(0,2.5),
+plt.plot([average,average],[0,3],'r--')
+plt.annotate("Mean = "+str(average), xy=(20, 2.5), xytext=(3,2.5),
              color="red", weight="light", fontsize=14,
              #arrowprops={"facecolor": "red"}
              )
 
-plt.annotate("RMS ="+str(rms), xy=(20, 2.5), xytext=(0,2.2),
+plt.annotate("RMS ="+str(rms), xy=(20, 2.5), xytext=(3,2.2),
              color="red", weight="light", fontsize=14,
              #arrowprops={"facecolor": "red"}
              )
@@ -80,13 +106,14 @@ plt.close()
 
 
 plt.plot(grades,home_work,"ko")
-plt.xlabel("Grades")
-plt.ylabel("Homework")
+plt.xlim(10, 50)
+
+plt.xlabel("MidTerm (XX/50)")
+plt.ylabel("Homework (%)")
 #plt.plot(,"ko")
 plt.savefig("Correlation.pdf")
 plt.savefig("Correlation.png")
 plt.close()
-
 
 binsCombined = np.linspace(0,100,50)
 plt.hist(combined, bins=binsCombined,color='b', alpha=0.65, histtype='stepfilled', label='stepfilled hist')
